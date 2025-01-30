df1132b65db07703a57feb514f9a3c01b60f7ccc
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2025-01-30 16:25:35 +0200

    Create MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+try:
+  
+# Database connection details (replace with your own)
+mydb = mysql.connector.connect(
+    host ="122.0.0.1",
+    userv="root",
+    password ="seventy77seven",
+    database ="alx_book_store"
+)
+
+if mydb.is_connected():
+  print('ALX_BOOK_STORE WORKS SUCCESSFULLY!')
+else 
+  cursor mydb.cursor()
+  cursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
+except mysql.Connector.Error as err:
+  print('Something went wrong:{}'.format(err))
+
+
